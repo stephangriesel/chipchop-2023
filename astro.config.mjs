7d9d35a3bb9d367fcdf603fa4aabb3b69b2f8822
Stephan Griesel <sgriesel@gmail.com>
2024-05-15 06:03:25 +0200

    change fav to boolean option

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,53 +1,67 @@
-import { defineConfig } from 'astro/config';
-import NetlifyCMS from 'astro-netlify-cms';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import NetlifyCMS from "astro-netlify-cms";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind({
-      config:{
-        path: './custom-config.cjs'
-      }
+      config: {
+        path: "./custom-config.cjs",
+      },
     }),
     NetlifyCMS({
       config: {
         // Use Netlify’s “Git Gateway” authentication and target our default branch
         backend: {
-          name: 'git-gateway',
-          branch: 'latest',
+          name: "git-gateway",
+          branch: "latest",
         },
         // Logo for backend
         logo_url: `https://res.cloudinary.com/stephangriesel/image/upload/v1687318146/logo_cqhhz9.png`,
         // Configure where our media assets are stored & served from
-        media_folder: 'public/upload',
-        public_folder: '/upload',
+        media_folder: "public/upload",
+        public_folder: "/upload",
         // Configure the content collections
         collections: [
           {
-            name: 'galleries',
-            label: 'Gallery Posts',
-            label_singular: 'Gallery Post',
-            folder: 'src/pages/posts',
+            name: "galleries",
+            label: "Gallery Posts",
+            label_singular: "Gallery Post",
+            folder: "src/pages/posts",
             create: true,
             delete: true,
             fields: [
-              { name: 'title', widget: 'string', label: 'Post Title' },
-              { 
-                name: 'thumbnail', 
-                widget: 'image', 
-                choose_url: true, 
-                default: '/upload/logogray.svg',
-              },
-              { name: 'description', widget: 'string', label: 'Description', required: true },
-              { name: 'tag', widget: 'string', label: 'Tag', required: true },
-              { name: 'body', widget: 'markdown', label: 'Post Body' },
+              { name: "title", widget: "string", label: "Post Title" },
               {
-                name: 'layout',
-                widget: 'select',
-                default: '../../layouts/GalleryPost.astro',
+                name: "starredTag",
+                widget: "boolean",
+                label: "Starred Project",
+                default: "false",
+              },
+              {
+                name: "thumbnail",
+                widget: "image",
+                choose_url: true,
+                default: "/upload/logogray.svg",
+              },
+              {
+                name: "description",
+                widget: "string",
+                label: "Description",
+                required: true,
+              },
+              { name: "tag", widget: "string", label: "Tag", required: true },
+              { name: "body", widget: "markdown", label: "Post Body" },
+              {
+                name: "layout",
+                widget: "select",
+                default: "../../layouts/GalleryPost.astro",
                 options: [
-                  { label: 'Gallery Post', value: '../../layouts/GalleryPost.astro' },
+                  {
+                    label: "Gallery Post",
+                    value: "../../layouts/GalleryPost.astro",
+                  },
                 ],
               },
             ],
