b9732127dd16f65935a60cd16ab7b579ebf5302f
Stephan Griesel <sgriesel@gmail.com>
2024-05-16 06:16:50 +0200

    add hint to starred config

--- astro.config.mjs
+++ astro.config.mjs
@@ -37,6 +37,7 @@ export default defineConfig({
                 name: "starredTag",
                 widget: "boolean",
                 label: "Starred Project",
+                hint: "Enabling this will add this photo to top. Minimum 3 photos required.",
                 default: "false",
               },
               {
