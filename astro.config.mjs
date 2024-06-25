9b50218de3911fc21a7997520ec9e09eabb2115d
Stephan Griesel <sgriesel@gmail.com>
2024-06-25 06:32:23 +0200

    hint update to project

--- astro.config.mjs
+++ astro.config.mjs
@@ -37,7 +37,7 @@ export default defineConfig({
                 name: "starredTag",
                 widget: "boolean",
                 label: "Featured Project",
-                hint: "Enabling this will add photo to top. Minimum 3 photos required.",
+                hint: "Enabling this will add photo to top. Minimum 3 projects required.",
                 default: "false",
               },
               {
