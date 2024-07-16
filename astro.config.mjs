be8e77a709ad264c4dc2ffd34d68663d60943fe0
Stephan Griesel <sgriesel@gmail.com>
2024-07-16 07:23:17 +0200

    config update for aria change

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,13 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  theme: {
+    extend: {
+      aria: {
+        current: "current=page",
+      },
+    },
+  },
   integrations: [
     tailwind({
       config: {
